9175c1b141fb001186ca57a6244517b053537cb9
kubikal7 <skubikal@gmail.com>
2025-04-10 17:05:57 +0200

    fix

--- src/main/java/com/example/BikeWeb/model/User.java
+++ src/main/java/com/example/BikeWeb/model/User.java
@@ -21,7 +21,6 @@ public class User {
     private String name;
     private String surname;
     private String email;
-    @JsonIgnore
     private String password;
     @JsonIgnore
     private String token;
